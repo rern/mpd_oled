11124e95c0cc416dd804ba1967522eb34a2a001a
rern <rernrern@gmail.com>
2026-05-02 16:09:28 +0700

    u

--- src/status.cpp
+++ src/status.cpp
@@ -56,6 +56,9 @@ const iconvpp::converter conv("ASCII//TRANSLIT", "UTF-8", true);
 static string to_ascii(string str)
 {
   string ascii;
+  if (str.empty()) {
+    return "";
+  }
   try {
     conv.convert(str, ascii);
   }
